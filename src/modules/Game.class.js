6c39da174a65cc6d2a1db94f41306416c645a553
Mykola <nyr3mvneov@gmail.com>
2025-09-01 20:53:43 +0300

    Added random cell addition

--- src/modules/Game.class.js
+++ src/modules/Game.class.js
@@ -32,6 +32,13 @@ class Game {
     this.gameField = document.querySelector('.game-field');
     this.buttonStart = document.querySelector('.button.start');
 
+    this.board = initialState || [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+
     this.buttonStart.addEventListener('click', () => {
       if (this.gameStatus === 'playing') {
         this.restart();
@@ -54,7 +61,9 @@ class Game {
   /**
    * @returns {number[][]}
    */
-  getState() {}
+  getState() {
+    return this.board;
+  }
 
   /**
    * Returns the current game status.
@@ -66,16 +75,28 @@ class Game {
    * `win` - the game is won;
    * `lose` - the game is lost
    */
-  getStatus() {}
+  getStatus() {
+    return this.gameStatus;
+  }
 
   /**
    * Starts the game.
    */
   start() {
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
     this.gameStatus = 'playing';
     startMessage.classList.add('hidden');
     this.buttonStart.classList.add('restart');
     this.buttonStart.textContent = 'Restart';
+
+    this.addRandomCell();
+    this.addRandomCell();
+    this.updateBoard();
   }
 
   /**
@@ -87,9 +108,69 @@ class Game {
     startMessage.classList.remove('hidden');
     this.buttonStart.classList.remove('restart');
     this.buttonStart.textContent = 'Start';
+
+    this.board = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    this.updateBoard();
   }
 
-  // Add your own methods here
+  getEmptyCells() {
+    const emptyCells = [];
+
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        if (this.board[row][col] === 0) {
+          emptyCells.push({ row, col });
+        }
+      }
+    }
+
+    return emptyCells;
+  }
+
+  getRandomValue() {
+    const randomNumber = Math.random();
+
+    return randomNumber < 0.1 ? 4 : 2;
+  }
+
+  addRandomCell() {
+    const emptyCells = this.getEmptyCells();
+
+    if (emptyCells.length === 0) {
+      return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * emptyCells.length);
+    const { row, col } = emptyCells[randomIndex];
+
+    this.board[row][col] = this.getRandomValue();
+  }
+
+  updateBoard() {
+    const tableRows = this.gameField.querySelectorAll('.field-row');
+
+    this.board.forEach((rowValues, rowIndex) => {
+      const fieldCells = tableRows[rowIndex].querySelectorAll('.field-cell');
+
+      rowValues.forEach((cellValue, colIndex) => {
+        const cellElement = fieldCells[colIndex];
+
+        cellElement.textContent = '';
+        cellElement.className = 'field-cell';
+
+        if (cellValue !== 0) {
+          cellElement.textContent = cellValue;
+          cellElement.classList.add(`tile`);
+          cellElement.classList.add(`field-cell--${cellValue}`);
+        }
+      });
+    });
+  }
 }
 
 module.exports = Game;
